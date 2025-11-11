87eb33360130c56ccffde3dad0c5a0e384b5f173
Pranav Sure <prann1234556@gmail.com>
2025-11-11 16:17:13 -0500

    hello

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
@@ -1,16 +1,23 @@
-//package org.firstinspires.ftc.teamcode.subsystems;
-//
-//import com.qualcomm.robotcore.hardware.DcMotor;
-//import com.qualcomm.robotcore.hardware.HardwareMap;
-//import com.seattlesolvers.solverslib.command.SubsystemBase;
-//import com.seattlesolvers.solverslib.hardware.motors.Motor;
-//
-//public class Intake extends SubsystemBase {
-//    private final Motor intakeMotor;
-//
-//    public Intake(final HardwareMap hardwareMap, final String name){
-//       intakeMotor = new Motor(hardwareMap, name);
-//    }
-//
-//    public intake(final)
-//}
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.seattlesolvers.solverslib.command.SubsystemBase;
+import com.seattlesolvers.solverslib.hardware.motors.Motor;
+
+public class Intake extends SubsystemBase {
+    private final Motor intakeMotor;
+
+    public Intake(final HardwareMap hardwareMap, final String name){
+       intakeMotor = new Motor(hardwareMap, name);
+       int p = 10;
+       int i = 3;
+       intakeMotor.setRunMode(Motor.RunMode.RawPower);
+    }
+    public void spin(){
+        intakeMotor.set(0.7);
+    }
+    public void reverse(){
+        intakeMotor.set(0.7);
+    }
+}
