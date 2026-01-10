3c54d0179cf46e1b9af6c7a4fb280a2f274016af
Trial N' Error <trialnerror42@gmail.com>
2026-01-10 00:55:51 -0500

    hello

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Shooter.java
@@ -1,51 +1,21 @@
-package org.firstinspires.ftc.teamcode.subsystems;
+public class ShooterSubsystem implements SubsystemBase {
+    public ShooterSubsystem(){
 
-import dev.nextftc.core.commands.Command;
-import dev.nextftc.core.commands.utility.InstantCommand;
-import dev.nextftc.core.commands.utility.NullCommand;
-import dev.nextftc.core.subsystems.Subsystem;
-import dev.nextftc.hardware.impl.MotorEx;
-import dev.nextftc.hardware.powerable.SetPower;
+    }
 
-/**
- * The Shooter subsystem is responsible for controlling the robot's shooter mechanism.
- * It uses a single motor to launch items.
- */
-public class Shooter implements Subsystem {
-    /**
-     * Singleton instance of the Shooter subsystem.
-     */
-    public static final Shooter INSTANCE = new Shooter();
+    public void setServos(){
 
+    }
 
-    /**
-     * Private constructor to prevent instantiation outside of the singleton.
-     */
-    private Shooter() {}
+    public void initializeServos(){
 
-    /**
-     * The motor for the shooter system.
-     */
-    private final MotorEx shooter_motor = new MotorEx("motorS").reversed();
+    }
+    public void highSpeed(){
 
+    }
 
-    /**
-     * Runnable to set the shooter to high speed.
-     */
-    public Command highSpeed = new SetPower(shooter_motor, 0.75).requires(this);
-    /**
-     * Runnable to set the shooter to normal speed. (this is adjustable speed for the tuning in order to accurately shoot)
-     */
-    public Command normalSpeed = new SetPower(shooter_motor, 0.5).requires(this);
-    /**
-     * Runnable to set the shooter to low speed.
-     */
-    public Command lowSpeed = new SetPower(shooter_motor, 0.3).requires(this);
+    public void stop(){
 
-
-    /**
-     * Command to stop the shooter motor.
-     */
-    public Command stop = new SetPower(shooter_motor, 0.0).requires(this);
-
+    }
+    public void
 }
