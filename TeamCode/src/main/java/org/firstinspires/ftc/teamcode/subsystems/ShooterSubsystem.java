aaafef9a54b65c2e9bc998d9aa19bc14f213cecc
Trial N' Error <trialnerror42@gmail.com>
2026-01-10 04:48:14 -0500

    nextftc -> solverslib

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ShooterSubsystem.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.seattlesolvers.solverslib.command.SubsystemBase;
+
+public class ShooterSubsystem extends SubsystemBase {
+    public ShooterSubsystem(final HardwareMap hMap){
+        servoLeft = hMap.get(Servo.class, "servoLeft");
+        servoRight = hMap.get(Servo.class, "servoRight");
+        motor
+    }
+
+    public void setServos(){
+
+    }
+
+    public void initializeServos(){
+
+    }
+    public void highSpeed(){
+
+    }
+
+    public void stop(){
+
+    }
+    public void
+}
